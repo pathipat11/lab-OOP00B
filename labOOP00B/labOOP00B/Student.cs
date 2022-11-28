34015db0913929977e2e74623eb8305aa35584b6
pathipat11 <119285292+pathipat11@users.noreply.github.com>
2022-11-28 16:56:14 +0700

    add avgweight program

---
+++ labOOP00B/labOOP00B/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labOOP00B
+{
+    public class Student
+    {
+        public string name;
+        public float weight;
+    }
+}
